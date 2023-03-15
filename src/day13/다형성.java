831893c6312f98177fd0430cb5c8335873c2ce4e
juni1015 <jiyoon6293@naver.com>
2023-03-16 08:20:42 +0900

    day13다형성 코드정리

--- src/day13/다형성.java
+++ src/day13/다형성.java
@@ -4,28 +4,32 @@ public class 다형성 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		Animal a = new Dog();
-//		
-//		a.eat();
-//		a.cry();
-		
+		// Animal a = new Dog();
+		//
+		// a.eat();
+		// a.cry();
+
 		Animal a = new Dog();
-		//다형성 : 부모타입의 객체에 자식 생성자로 생성할 수 있는 것
-		//특징 : Animal 객체이기 때문에 Animal 다 사용 가능하고 
-		//		자식클래스는 오버라이딩 된 것만 사용 가능함
-		
-//		test(a);
-//		a.a = new Dog();
-	}
-	
-	public static void test(Animal a) {
 		a.cry();
+		a.eat();
+		// a.cute(); //cute는 오버라이딩된 메소드가 아닌 관계로 사용할 수 없음 / 오류 발생
+		
+		Dog b = new Dog();
+		b.cute();
+		// test(a);
+		// a.a = new Dog();
+		// 다형성 : 부모타입의 객체에 자식 생성자로 생성할 수 있는 것
+		// 특징 : Animal 객체이기 때문에 Animal 다 사용 가능하고
+		// 자식클래스는 오버라이딩 된 것만 사용 가능함
 	}
 
+	// public static void test(Animal a) {
+	// a.cry();
+	// }
 }
 
 class Animal {
-//	Animal a = new Animal();
+	// Animal a = new Animal();
 	public void eat() {
 		System.out.println("먹다");
 	}
@@ -33,11 +37,16 @@ class Animal {
 		System.out.println("울다");
 	}
 }
+
 class Dog extends Animal {
 	public void cry() {
 		System.out.println("멍멍");
 	}
+	public void cute() {
+		System.out.println("귀여워");
+	}
 }
+
 class Cat extends Animal {
 	public void cry() {
 		System.out.println("야옹");
