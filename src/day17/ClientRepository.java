6379ead510d0308b87a5b0bfa23068db44972618
juni1015 <jiyoon6293@naver.com>
2023-03-16 17:41:45 +0900

    day13에만든 은행폼 Client list를 Map으로 변경 및 아이디로 검색 기능 추가

---
+++ src/day17/ClientRepository.java
@@ -0,0 +1,149 @@
+package day17;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ClientRepository {
+	private static ClientRepository repository = new ClientRepository();
+	private ClientRepository() {}
+	public static ClientRepository getInstence() {
+		return repository;
+	}
+	
+//	List<ClientDTO> cList = new ArrayList<>();
+	Map<String, ClientDTO> cMap = new HashMap<>();
+	List<BreakdownDTO> bList = new ArrayList<>();
+	
+	public boolean save(ClientDTO clientDTO) {
+//		return cList.add(clientDTO);
+		cMap.put(clientDTO.getAccount(), clientDTO);
+		if(cMap.get(clientDTO.getAccount()) == null) {
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean loginChenk(String id, String password) {
+		for(String key : cMap.keySet()) {
+			if(id.equals(cMap.get(key).getId()) && password.equals(cMap.get(key).getPassword())) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public Map<String, ClientDTO> findAll() {
+		return cMap;
+	}
+	
+	public ClientDTO findById(String id, String password) {
+		for(String key : cMap.keySet()) {
+			if(id.equals(cMap.get(key).getId()) && password.equals(cMap.get(key).getPassword())) {
+				return cMap.get(key);
+			}
+		}
+		return null;
+	}
+	
+	public List<BreakdownDTO> breakList(String account) {
+		List<BreakdownDTO> list = new ArrayList<>();
+		for(BreakdownDTO b : bList) {
+			if(b.getAccount().equals(account)) {
+				list.add(b);
+			}
+		}
+		return list;
+	}
+	
+	public String getAccount(String id, String password) {
+		for(String key : cMap.keySet()) {
+			if(id.equals(cMap.get(key).getId())&& password.equals(cMap.get(key).getPassword())) {
+				return cMap.get(key).getAccount();
+			}
+		}
+		return null;
+	}
+	
+	public boolean deposit(String account, long money) {
+		for(String key : cMap.keySet()) {
+			if(account.equals(key)) {	
+				//account.equals(cMap.get(key).getAccount())로 작성해도 되지만 key가 account의 값을 가지고 있기 때문에 key로 작성해도 무관함
+				cMap.get(key).setBalance(cMap.get(key).getBalance() + money);
+				BreakdownDTO breakdownDTO = new BreakdownDTO();	//거래내역을 넣을 객체 생성
+				breakdownDTO.setAccount(account);
+				breakdownDTO.setDivision("입금");
+				breakdownDTO.setDealMoney(money);
+				breakdownDTO.setTotalMoney(cMap.get(key).getBalance());
+				bList.add(breakdownDTO);
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean transferChenk(String transferAccount) {
+		for(String key : cMap.keySet()) {
+			if(transferAccount.equals(key)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public int withdraw(String account, long money) {
+		for(String key : cMap.keySet()) {
+			if(account.equals(key)) {
+				if(cMap.get(key).getBalance() >= money) {
+					cMap.get(key).setBalance(cMap.get(key).getBalance() - money);
+					BreakdownDTO breakdownDTO = new BreakdownDTO();
+					breakdownDTO.setAccount(account);
+					breakdownDTO.setDivision("출금");
+					breakdownDTO.setDealMoney(money);
+					breakdownDTO.setTotalMoney(cMap.get(key).getBalance());
+					bList.add(breakdownDTO);
+					return 1;
+				} else {
+					return 0;
+				}
+			}
+		}
+		return -1;
+	}
+	
+	public boolean update(String id, String password, String updatePassword) {
+		for(String key : cMap.keySet()) {
+			if(id.equals(cMap.get(key).getId()) && password.equals(cMap.get(key).getPassword())) {
+				cMap.get(key).setPassword(updatePassword);
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean delete(String id, String password) {
+		for(String key : cMap.keySet()) {
+			if(id.equals(cMap.get(key).getId()) && password.equals(cMap.get(key).getPassword())) {
+				cMap.remove(key);
+				return true;
+			} 
+		}
+		return false;
+	}
+	
+	public List<ClientDTO> select(String selectId) {
+		List<ClientDTO> selectList = new ArrayList<>();
+		//검색 정렬
+		List<String> keySet = new ArrayList<>(cMap.keySet());
+		Collections.sort(keySet);
+		
+		for(String key : keySet) {
+			if(selectId.equals(cMap.get(key).getId())) {
+				selectList.add(cMap.get(key));
+			}
+		}
+		return selectList;
+	}
+}
---
+++ src/day17/ClientService.java
@@ -0,0 +1,204 @@
+package day17;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class ClientService {
+	//싱글톤패턴
+	private static ClientService service = new ClientService();	
+	private ClientService() {}	//생성자
+	public static ClientService getIstence() {
+		return service;
+	}
+	
+	private Scanner sc = new Scanner(System.in);
+	private ClientRepository repository = ClientRepository.getInstence();
+	private String loginId = null;	//세션역할
+	private String loginPassword = null;
+	
+	public void save() {
+		ClientDTO clientDTO = new ClientDTO();
+		System.out.print("id> ");
+		clientDTO.setId(sc.next());
+		System.out.print("password> ");
+		clientDTO.setPassword(sc.next());
+		System.out.print("name> ");
+		clientDTO.setName(sc.next());
+		if(repository.save(clientDTO)) {
+			System.out.println("회원가입 성공");
+		} else {
+			System.out.println("회원가입 실패");
+		}
+	}
+	
+	public boolean loginCheck() {
+		System.out.print("id> ");
+		String id = sc.next();
+		System.out.print("password> ");
+		String password = sc.next();
+		
+		if(repository.loginChenk(id, password)) {
+			loginId = id;
+			loginPassword = password;
+			System.out.println("로그인 성공");
+			return true;
+		} else {
+			System.out.println("로그인 실패");
+			return false;
+		}
+	}
+	
+	public void findAll() {
+		Map<String, ClientDTO> cMap = repository.findAll();
+		System.out.println("계좌번호\t\t아이디\t패스워드\t예금주\t잔액\t가입일");
+		System.out.println("------------------------------------------------------------------");
+		
+		//정렬
+		List<String> keySet = new ArrayList<>(cMap.keySet());
+		Collections.sort(keySet);
+		
+		for(String key : keySet) {
+			System.out.println(cMap.get(key).toString());
+		}
+	}
+	
+	public void findById() {
+		ClientDTO clientDTO = repository.findById(loginId, loginPassword);
+		if(clientDTO == null) {
+			System.out.println("로그인 오류");
+		} else {
+			System.out.println("계좌번호\t\t아이디\t패스워드\t예금주\t잔액\t가입일");
+			System.out.println("------------------------------------------------------------------");
+			System.out.println(clientDTO.toString());
+			System.out.println("------------------------------------------------------------------");
+			List<BreakdownDTO> bList  = repository.breakList(clientDTO.getAccount());
+			if(bList.size() == 0) {
+				System.out.println("입출금 내역이 없습니다");
+			} else {				
+				System.out.println("▼입출금내역▼");
+				System.out.println("------------------------------------------------------------------");
+				System.out.println("계좌번호\t\t구분\t거래금액\t거래후 잔액\t거래일");
+				for(BreakdownDTO b : bList) {
+					System.out.println(b.toString());
+				}
+				System.out.println("------------------------------------------------------------------");
+			}
+		}
+		
+	}
+	
+	public void deposit() {
+		String account = repository.getAccount(loginId, loginPassword);
+		if(account == null) {
+			System.out.println("로그인 오류");
+		} else {
+			System.out.print("입금금액> ");
+			long money = sc.nextLong();
+			if(repository.deposit(account, money)) {
+				System.out.println("입금완료");
+			} else {
+				System.out.println("입금실패");
+			}
+		}
+		
+	}
+	
+	public void withdraw() {
+		String account = repository.getAccount(loginId, loginPassword);
+		if(account == null) {
+			System.out.println("로그인 오류");
+		} else {
+			System.out.print("출금금액> ");
+			long money = sc.nextLong();
+			int ok = repository.withdraw(account, money);
+			if(ok == 1) {
+				System.out.println("출금완료");
+			} else if(ok == 0) {
+				System.out.println("잔액부족");
+			} else {
+				System.out.println("계좌오류");
+			}
+		}
+	}
+	
+	public void transfer() {
+		String wAccount = repository.getAccount(loginId, loginPassword);
+		if(wAccount == null) {
+			System.out.println("출금계좌 오류");
+			return;
+		}
+		System.out.print("이체할 계좌번호> ");
+		String transferAccount = sc.next();
+		if(repository.transferChenk(transferAccount)) {
+			System.out.print("이체할 금액> ");		//계좌이체 있는지 먼저 확인하기 위해 위치 if문 아래로 변경함
+			long transferMoney = sc.nextLong();
+			int ok = repository.withdraw(wAccount, transferMoney);
+			if(ok == 1) {
+				System.out.println("출금완료");
+				if(repository.deposit(transferAccount, transferMoney)) {
+					System.out.println("입금완료");
+				}
+			} else if(ok == 0) {
+				System.out.println("잔액부족");
+			} else {
+				System.out.println("계좌오류");
+			}
+		} else {
+			System.out.println("이체할 계좌번호가 없습니다.");
+		}
+	}
+	
+	public void update() {
+		System.out.print("패스워드 확인> ");
+		String password = sc.next(); 
+		
+		if(password.equals(loginPassword)) {
+			System.out.print("수정할 패스워드> ");
+			String updatePassword = sc.next();
+			if(repository.update(loginId, loginPassword, updatePassword)) {
+				loginPassword = updatePassword;
+				System.out.println("패스워드 수정완료");
+			} else {
+				System.out.println("패스워드 수정실패");
+			}
+		} else {
+			System.out.println("패스워드가 일치하지 않습니다.");
+		}
+	}
+	
+	public boolean delete() {
+		if(repository.delete(loginId, loginPassword)) {
+			System.out.println("삭제성공");
+			return false;
+		} else {
+			System.out.println("삭제실패");
+			return true;
+		}
+	}
+	
+	public void logout() {
+		loginId = null;
+		loginPassword = null;
+		System.out.println("로그아웃");
+	}
+	
+	public void select() {
+		System.out.print("검색할 아이디> ");
+		String selectId = sc.next();
+		sc.nextLine();
+		List<ClientDTO> selectList = repository.select(selectId);
+		if(selectList.size() == 0) {
+			System.out.println("없는 아이디입니다.");
+		} else {
+			System.out.println("계좌번호\t\t아이디\t패스워드\t예금주\t잔액\t가입일");
+			System.out.println("------------------------------------------------------------------");
+			for(ClientDTO c : selectList) {
+				System.out.println(c.toString());
+			}
+		}
+	}
+
+}
