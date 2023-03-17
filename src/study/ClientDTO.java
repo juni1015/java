b835c5a508f3432e95cb096c87ca292c6b1f3d60
juni1015 <jiyoon6293@naver.com>
2023-03-17 18:25:24 +0900

    공부용 은행폼 개발 시작

--- src/study/ClientDTO.java
+++ src/study/ClientDTO.java
@@ -1,5 +1,23 @@
 package study;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 public class ClientDTO {
+	private static int number = 1000;
+	private final static DateTimeFormatter DTF = DateTimeFormatter.ofPattern("yy/MM/dd hh:mm:ss");
+	private String id;
+	private String pw;
+	private String name;
+	private int purpose;	//목적
+	private Date joinDate;
+	private String acoount;
+	
+	public void ClientDTO() {
+//		this.joinDate = DTF.date
+	}
+	
+	
 
 }
